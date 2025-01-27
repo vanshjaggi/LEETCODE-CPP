60b6cea778f4caeee0ecc933f6f5dd07d5969b71
Vansh Jaggi <jaggivansh4@gmail.com>
2025-01-27 23:18:32 +0530

    some tweaks to regularexpressionmatching.cpp

--- regularexpressionmatching.cpp
+++ regularexpressionmatching.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int n = s.length(), m = p.length();
-        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
-        
-        dp[0][0] = true;
-        
-        for (int j = 1; j <= m; ++j) {
-            if (p[j - 1] == '*') {
-                dp[0][j] = dp[0][j - 2];
+        vector<vector<bool>> dp(s.length()+1,vector<bool>(p.length()+1,false));
+        dp[0][0]=true;
+
+        for(int i = 1;i<=p.length();i++){
+            if(p[i-1]=='*'){
+                dp[0][i] = dp [0][i-2];
             }
         }
-        
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= m; ++j) {
-                if (p[j - 1] == s[i - 1] || p[j - 1] == '.') {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else if (p[j - 1] == '*') {
-                    dp[i][j] = dp[i][j - 2] || (dp[i - 1][j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.'));
-                } else {
-                    dp[i][j] = false;
+        for(int i = 1; i<=s.length();i++){
+            for(int j = 1; j<=p.length();j++){
+                if(s[i-1]==p[j-1] || p[j-1]=='.'){
+                    dp[i][j] = dp[i-1][j-1];
+                }else if(p[j-1]=='*'){
+                    dp[i][j] = dp[i][j-2] || (dp[i-1][j] && ((s[i-1] == p[j-2]) || p[j-2] == '.'));
                 }
             }
         }
-        return dp[n][m];
+        return dp[s.length()][p.length()];
     }
 };
