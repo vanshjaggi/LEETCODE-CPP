2c3a88dc8a92b72d9c51ad791c0427f3a991f6f7
vanshjaggi <144230165+vanshjaggi@users.noreply.github.com>
2025-02-05 17:01:46 +0530

    32nd Question

---
+++ CheckifOneStringSwapCanMakeStringsEqual.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        if(s1 == s2) return true;
+        vector<int> diff;
+        for(int i = 0; i<s1.length(); i++){
+            if(s1[i] != s2[i]){
+                diff.push_back(i);
+                if(diff.size() > 2) return false;
+            }
+        }
+        return diff.size() == 2 && s1[diff[0]] == s2[diff[1]] && s1[diff[1]] == s2[diff[0]];
+    }
+};
